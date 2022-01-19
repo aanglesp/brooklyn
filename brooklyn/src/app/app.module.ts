785043881bbf57e26d23d7a99b436a9d29fbbca2
aanglesp <alexanderaaap@gmail.com>
2022-01-18 19:13:36 -0500

    headers and part of body

--- brooklyn/src/app/app.module.ts
+++ brooklyn/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainToolbarComponent } from './main-toolbar/main-toolbar.component';
+import { SecondToolbarComponent } from './second-toolbar/second-toolbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainToolbarComponent,
+    SecondToolbarComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
---
+++ brooklyn/src/app/second-toolbar/second-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second-toolbar',
+  templateUrl: './second-toolbar.component.html',
+  styleUrls: ['./second-toolbar.component.scss']
+})
+export class SecondToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
